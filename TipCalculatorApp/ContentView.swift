985047b9667bd661e4ce1b348e7b75ffbc60ff7f
TormentRonish <ronishtorment@gmail.com>
2025-01-29 17:45:41 -0600

    Initial Commit

---
+++ TipCalculatorApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipCalculatorApp
+//
+//  Created by Ronish, Torment on 1/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipCalculatorApp/TipCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorAppApp.swift
+//  TipCalculatorApp
+//
+//  Created by Ronish, Torment on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
